a0f6fdd2d807f953212cd266f347cd3672dbadf6
Richard Darst <rkd@zgib.net>
2014-08-25 23:42:49 +0300

    Add tests.  Tests consider making a dataset and running CD method.

--- tests.py
+++ tests.py
@@ -5,12 +5,63 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import os
+import shutil
+
 from django.test import TestCase
+from django.test.utils import override_settings
 
+from . import models
+from . import utils
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BasicTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        from . import config, models
+        cls.ROOTDIR = config.ROOTDIR+'/test-base/'
+        config.ROOTDIR_OLD = config.ROOTDIR
+        config.ROOTDIR = models.ROOTDIR = cls.ROOTDIR
+        os.mkdir(config.ROOTDIR)
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.ROOTDIR)
+        from . import config, models
+        config.ROOTDIR = models.ROOTDIR = config.ROOTDIR_OLD
+
+    def test_basic(self):
+        self.client.get('/')
+        #self.client.get('/dataset/20/')
+        self.assertRaises(Exception, self.client.get, '/dataset/20/')
+
+        # Make test dataset
+        ds = models.Dataset(id=20)
+        ds.save()
+        r = self.client.get('/dataset/20/')
+        #print r.content
+        self.assertContains(r, u'Current network: None.', )
+        assert 'Current network: None.' in r.content
+
+        ds.netfile = utils.get_graph_file('data/karate.gml')
+        r = self.client.post('/dataset/20/',
+                             dict(netfile=utils.get_graph_file('data/karate.gml'),
+                                  nettype='auto'))
+        self.assertContains(r, u'karate.gml')
+        r = self.client.get('/dataset/20/')
+        self.assertContains(r, u'karate.gml')
+
+        r = self.client.post('/dataset/20/',
+                             dict(cdname='Copra'))
+        self.assertRedirects(r, '/dataset/20/Copra/', status_code=302)
+
+        r = self.client.get('/dataset/20/Copra/')
+        self.assertContains(r, 'Copra')
+        self.assertContains(r, 'not completed')
+
+        # Run CD
+        r = self.client.post('/dataset/20/Copra/',
+                             dict(weighted=False, max_overlap=1, trials=10))
+        #self.assertContains(r, 'state: D')
+        #print r.content
+        r = self.client.get('/dataset/20/Copra/')
+
+        self.assertContains(r, 'state: D')
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import os
+from os.path import dirname, join
 import textwrap
+
+from django.core.files.base import ContentFile
 
 def dedent(s):
     """Like textwrap.dedent, but skips the first line.
@@ -27,3 +31,20 @@ class ListField(CharField):
             return [ self._type(x) for x in data ]
         except ValueError as e:
             raise ValidationError("Invalid type: %s"%e)
+
+
+
+def get_graph_file(path, name=None):
+    """Get graph file in Django object for testing purposes"""
+    fname = join(dirname(__file__), path)
+    # We must use ContentFile to avoid SuspiciousOperation errors.
+    # We read the data outside of django and pass it as a string.
+    f = ContentFile(open(fname).read())
+    if name is None:
+        name = os.path.basename(path)
+    f.name = name
+    f.path = name
+    return f
+def set_graph_file(ds, f):
+    if ds.netfile: ds.netfile.delete()
+    ds.set_network(f)
