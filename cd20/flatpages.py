6817419b86321dbab769fad92f838d68e18a987a
Richard Darst <rkd@zgib.net>
2016-10-28 20:26:53 +0300

    Fix flatpages for django 1.8

--- cd20/flatpages.py
+++ cd20/flatpages.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 basedir = os.path.join(basedir, 'jako/pages')
 
 from django.template import Template, RequestContext
-from django.template.loader import get_template, find_template, get_template_from_string
+#from django.template.loader import get_template, find_template, get_template_from_string
 from django.http import HttpResponse
 
 def view_flat(request, pagename):
@@ -16,16 +16,5 @@ def view_flat(request, pagename):
 
     Custom-load templates from the jako/pages/ directory, and use this
     to render 'static' pages."""
-    #fname = join(basedir, pagename+'.html')
-    #body = open(fname).read()
-    #template = Template(body)
-    #return render(request, template, locals())
-
-    # In 1.7 this can be replaced by the get_template call with dirs=
-    # argument.
-    template, origin = find_template(pagename+'.html', dirs=[basedir])
-    #template = get_template_from_string(template, origin, pagename+'.html')
-
     pagetitle = title = pagename.title()
-
-    return HttpResponse(template.render(RequestContext(request, locals())))
+    return render(request, pagename+'.html', locals())
